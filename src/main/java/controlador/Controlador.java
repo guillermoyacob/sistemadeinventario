cbfd1c55e7478844cde4551076eb6274f3ee3c1a
Guillermo Yacob <guillermoyacob@gmail.com>
2022-10-22 20:40:17 -0300

    Modificando las dependencias

--- src/main/java/controlador/Controlador.java
+++ src/main/java/controlador/Controlador.java
@@ -1,4 +1,3 @@
-
 package controlador;
 
 import java.io.IOException;
@@ -14,21 +13,39 @@ import modeloDAO.InterfazProductoDAO;
 public class Controlador extends HttpServlet {
 
     InterfazProductoDAO dAO;
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String accion = request.getParameter("accion");
-        List<Producto> productos = null;
-        switch (accion) {
-            case "listar":
-                productos = dAO.getProductos();
-                request.setAttribute("Productos", productos);
-                request.getRequestDispatcher("index.jsp").forward(request, response);
-                break;
-            default:
-                throw new AssertionError();
+        
+        
+//        String accion = request.getParameter("accion");
+//        List<Producto> productos = null;
+//        switch (accion) {
+//            case "listar":
+//                productos = dAO.getProductos();
+//                request.setAttribute("Productos", productos);
+//                request.getRequestDispatcher("index.jsp").forward(request, response);
+//                break;
+//            default:
+//                throw new AssertionError();
+
+
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet NewServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            
+            
         }
     }
-
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -36,13 +53,11 @@ public class Controlador extends HttpServlet {
         processRequest(request, response);
     }
 
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
 
     @Override
     public String getServletInfo() {
