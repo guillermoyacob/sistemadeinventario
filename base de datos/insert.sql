a9fc772fe0b27d908927456b62d96dd948aafaf4
Guillermo Yacob <guillermoyacob@gmail.com>
2022-10-25 18:26:20 -0300

    Agregando archivo insert sql

---
+++ base de datos/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO producto (nombre, descripcion, unidades, costo, precio, categoria)
+VALUES
+	('Yerba Marolio', 'Suave 1kg', 5, 600, 800, 'Alimentos'),
+	('Azucar Chango', '1kg', 5, 180, 200, 'Alimentos'),
+	('Leche La Verónica', '1lt', 7, 130, 180, 'Alimentos');
